9e3561f8b707f231598bc0426c117516d2a8602f
Mohammad Amil Bhat <61305746+amilbhat@users.noreply.github.com>
2023-09-03 22:52:14 +0530

    Added Basic firebase authentication

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIHhCEhxHqOYCPOg9KVEbS4LtFuALs80c",
+  authDomain: "realtime-chat-4ca5e.firebaseapp.com",
+  projectId: "realtime-chat-4ca5e",
+  storageBucket: "realtime-chat-4ca5e.appspot.com",
+  messagingSenderId: "114256027694",
+  appId: "1:114256027694:web:ad62b3fe30775d9eca3fbf"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,15 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage, db } from "./../../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore"; 
+
 
 import classes from './Register.module.css'
 import addAvatarImg from "./../../assets/images/addAvatar.png"
 
 const Register = () => {
+    const [error, setError] = useState(false)
+
+    const handleSubmit = async (e) => {
+
+        e.preventDefault()
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
+        
+        try{
+            const response = await createUserWithEmailAndPassword(auth, email, password);
+
+            const storageRef = ref(storage, displayName);
+
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on('state_changed', 
+                (error) => {
+                    setError(true)
+                    console.log(error)
+                }, 
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then( async(downloadURL) => {
+                        await updateProfile(response.user,{
+                            displayName: displayName,
+                            photoURL: downloadURL,
+                        })
+                        // Error with FireStorage
+                        await setDoc(doc(db, 'users', response.user.uid), {
+                            uid: response.user.uid,
+                            displayName: displayName,
+                            email: email,
+                            photoURL: downloadURL
+                        })
+                    });
+                }
+            );
+        }
+        catch(err) {
+            setError(true)
+            console.log(err)
+        }
+
+
+    }
+
     return (
         <div className={classes.formContainer}>
             <div className={classes.formWrapper}>
                 <span className={classes.logo}>MAB Chat</span>
                 <span className={classes.title}>Register</span>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="display name" />
                     <input type="email" placeholder="email" />
                     <input type="password" placeholder="password" />
@@ -19,6 +71,7 @@ const Register = () => {
                         <span>Add an avatar</span>
                     </label>
                     <button>Sign Up</button>
+                    {error && <span>Something Went Wrong</span>}
                 </form>
                 <p>already have an account? Login</p>
             </div>
